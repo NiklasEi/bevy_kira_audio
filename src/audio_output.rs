c892363ddf1eed7b952cf8caea53507633fa434f
Niklas Eicker <git@nikl.me>
2021-01-17 23:25:15 +0100

    start from current bevy_audio :rocket:

---
+++ src/audio_output.rs
@@ -0,0 +1,74 @@
+use crate::{Audio, AudioSource, Decodable};
+use bevy::asset::Asset;
+use bevy::prelude::*;
+use rodio::{OutputStream, OutputStreamHandle, Sink};
+use std::marker::PhantomData;
+
+/// Used internally to play audio on the current "audio device"
+pub struct AudioOutput<P = AudioSource>
+where
+    P: Decodable,
+{
+    _stream: OutputStream,
+    stream_handle: OutputStreamHandle,
+    phantom: PhantomData<P>,
+}
+
+impl<P> Default for AudioOutput<P>
+where
+    P: Decodable,
+{
+    fn default() -> Self {
+        let (stream, stream_handle) = OutputStream::try_default().unwrap();
+
+        Self {
+            _stream: stream,
+            stream_handle,
+            phantom: PhantomData,
+        }
+    }
+}
+
+impl<P> AudioOutput<P>
+where
+    P: Asset + Decodable,
+    <P as Decodable>::Decoder: rodio::Source + Send + Sync,
+    <<P as Decodable>::Decoder as Iterator>::Item: rodio::Sample + Send + Sync,
+{
+    fn play_source(&self, audio_source: &P) {
+        let sink = Sink::try_new(&self.stream_handle).unwrap();
+        sink.append(audio_source.decoder());
+        sink.detach();
+    }
+
+    fn try_play_queued(&self, audio_sources: &Assets<P>, audio: &mut Audio<P>) {
+        let mut queue = audio.queue.write();
+        let len = queue.len();
+        let mut i = 0;
+        while i < len {
+            let audio_source_handle = queue.pop_back().unwrap();
+            if let Some(audio_source) = audio_sources.get(&audio_source_handle) {
+                self.play_source(audio_source);
+            } else {
+                // audio source hasn't loaded yet. add it back to the queue
+                queue.push_front(audio_source_handle);
+            }
+            i += 1;
+        }
+    }
+}
+
+/// Plays audio currently queued in the [Audio] resource through the [AudioOutput] resource
+pub fn play_queued_audio_system<P: Asset>(_world: &mut World, resources: &mut Resources)
+where
+    P: Decodable,
+    <P as Decodable>::Decoder: rodio::Source + Send + Sync,
+    <<P as Decodable>::Decoder as Iterator>::Item: rodio::Sample + Send + Sync,
+{
+    let audio_output = resources.get_thread_local::<AudioOutput<P>>().unwrap();
+    let mut audio = resources.get_mut::<Audio<P>>().unwrap();
+
+    if let Some(audio_sources) = resources.get::<Assets<P>>() {
+        audio_output.try_play_queued(&*audio_sources, &mut *audio);
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+mod audio;
+mod audio_output;
+mod audio_source;
+
+pub use audio::*;
+pub use audio_output::*;
+pub use audio_source::*;
+use bevy::prelude::{stage, AddAsset, AppBuilder, IntoSystem, Plugin};
+
+pub mod prelude {
+    pub use crate::{Audio, AudioOutput, AudioSource, Decodable};
+}
+
+/// Adds support for audio playback to an App
+#[derive(Default)]
+pub struct AudioPlugin;
+
+impl Plugin for AudioPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.init_thread_local_resource::<AudioOutput<AudioSource>>()
+            .add_asset::<AudioSource>()
+            .init_asset_loader::<Mp3Loader>()
+            .init_resource::<Audio<AudioSource>>()
+            .add_system_to_stage(
+                stage::POST_UPDATE,
+                play_queued_audio_system::<AudioSource>.system(),
+            );
+    }
+}
