ea78fb5409c2229484a0c5f9c2a22221c666de7a
Niklas Eicker <git@nikl.me>
2021-02-14 14:38:09 +0100

    Add possibility to loop a sound

--- src/audio.rs
+++ src/audio.rs
@@ -3,13 +3,28 @@ use bevy::prelude::Handle;
 use parking_lot::RwLock;
 use std::collections::VecDeque;
 
+pub struct PlayAudioSettings {
+    pub source: Handle<AudioSource>,
+    pub looped: bool,
+}
+
 #[derive(Default)]
 pub struct Audio {
-    pub queue: RwLock<VecDeque<Handle<AudioSource>>>,
+    pub queue: RwLock<VecDeque<PlayAudioSettings>>,
 }
 
 impl Audio {
     pub fn play(&self, audio_source: Handle<AudioSource>) {
-        self.queue.write().push_front(audio_source);
+        self.queue.write().push_front(PlayAudioSettings {
+            source: audio_source,
+            looped: false,
+        });
+    }
+
+    pub fn play_looped(&self, audio_source: Handle<AudioSource>) {
+        self.queue.write().push_front(PlayAudioSettings {
+            source: audio_source,
+            looped: true,
+        });
     }
 }
--- src/audio_output.rs
+++ src/audio_output.rs
@@ -5,6 +5,7 @@ use crate::source::AudioSource;
 use kira::manager::{AudioManager, AudioManagerSettings};
 use kira::sequence::Sequence;
 use kira::sound::handle::SoundHandle;
+use kira::Duration;
 use std::collections::HashMap;
 
 pub struct AudioOutput {
@@ -22,31 +23,56 @@ impl Default for AudioOutput {
 }
 
 impl AudioOutput {
+    fn get_or_create_sound(
+        &mut self,
+        audio_source: &AudioSource,
+        audio_source_handle: Handle<AudioSource>,
+    ) -> SoundHandle {
+        if let Some(handle) = self.sounds.get(&audio_source_handle) {
+            return handle.clone();
+        }
+
+        let sound = audio_source.sound.clone();
+        let handle = self.manager.add_sound(sound).unwrap();
+        self.sounds
+            .insert(audio_source_handle.clone(), handle.clone());
+        handle
+    }
+
+    fn play(&mut self, sound_handle: &SoundHandle) {
+        let mut sequence = Sequence::<()>::new(Default::default());
+        sequence.play(sound_handle, Default::default());
+        self.manager
+            .start_sequence(sequence, Default::default())
+            .unwrap();
+    }
+
+    fn play_looped(&mut self, sound_handle: &SoundHandle) {
+        let mut sequence = Sequence::<()>::new(Default::default());
+        sequence.start_loop();
+        sequence.play(sound_handle, Default::default());
+        sequence.wait(Duration::Seconds(sound_handle.duration()));
+        self.manager
+            .start_sequence(sequence, Default::default())
+            .unwrap();
+    }
+
     pub fn play_queued(&mut self, audio_sources: &Assets<AudioSource>, audio: &mut Audio) {
         let mut queue = audio.queue.write();
         let len = queue.len();
         let mut i = 0;
         while i < len {
-            let audio_source_handle = queue.pop_back().unwrap();
-
-            if let Some(audio_source) = audio_sources.get(&audio_source_handle) {
-                let handle = if let Some(handle) = self.sounds.get(&audio_source_handle) {
-                    handle
+            let play_settings = queue.pop_back().unwrap();
+            if let Some(audio_source) = audio_sources.get(&play_settings.source) {
+                let sound_handle = self.get_or_create_sound(audio_source, play_settings.source);
+                if play_settings.looped {
+                    self.play_looped(&sound_handle);
                 } else {
-                    let sound = audio_source.sound.clone();
-                    let handle = self.manager.add_sound(sound).unwrap();
-                    self.sounds
-                        .insert(audio_source_handle.clone(), handle.clone());
-                    self.sounds.get(&audio_source_handle).unwrap()
-                };
-                let mut sequence = Sequence::<()>::new(Default::default());
-                sequence.play(handle, Default::default());
-                self.manager
-                    .start_sequence(sequence, Default::default())
-                    .unwrap();
+                    self.play(&sound_handle);
+                }
             } else {
                 // audio source hasn't loaded yet. add it back to the queue
-                queue.push_front(audio_source_handle);
+                queue.push_front(play_settings);
             }
             i += 1;
         }
