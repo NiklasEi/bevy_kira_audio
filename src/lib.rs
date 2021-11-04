33b182bb05e3497955f9ebf12b3db75bbc91df1b
Niklas Eicker <git@nikl.me>
2021-11-04 22:42:50 +0100

    Compile examples in the readme as doc tests

--- src/lib.rs
+++ src/lib.rs
@@ -187,3 +187,15 @@ where
         );
     }
 }
+
+#[cfg(doctest)]
+mod test_readme {
+    macro_rules! external_doc_test {
+    ($x:expr) => {
+        #[doc = $x]
+        extern {}
+    };
+  }
+
+    external_doc_test!(include_str!("../README.md"));
+}
