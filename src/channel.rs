a1dac3e14d74425f8c7a4d425523f4173c178e75
eickern <git@nikl.me>
2022-05-07 22:57:44 +0200

    Remove AudioStreamChannel doc tests

--- src/channel.rs
+++ src/channel.rs
@@ -1,16 +1,6 @@
 /// A channel to play audio in
 ///
-/// You can play audio streams in this channel and control
-/// properties like the volume, playback rate or panning
-/// ```edition2018
-/// # use bevy_kira_audio::{AudioStreamChannel, Audio};
-/// # use bevy::prelude::*;
-///
-/// fn my_system(asset_server: Res<AssetServer>, audio: Res<Audio>) {
-///     let channel = AudioStreamChannel::new("my-channel".to_owned());
-///     audio.play_in_channel(asset_server.load("audio.mp3"), &channel);
-/// }
-/// ```
+/// You can play audio streams in this channel
 #[derive(PartialEq, Eq, Hash, Clone, Debug)]
 pub struct AudioStreamChannel {
     key: String,
@@ -26,16 +16,6 @@ impl Default for AudioStreamChannel {
 
 impl AudioStreamChannel {
     /// Create a new AudioChannel
-    ///
-    /// ```edition2018
-    /// # use bevy_kira_audio::{AudioStreamChannel, Audio};
-    /// # use bevy::prelude::*;
-    ///
-    /// fn my_system(asset_server: Res<AssetServer>, audio: Res<Audio>) {
-    ///     let channel = AudioStreamChannel::new("my-channel".to_owned());
-    ///     audio.play_in_channel(asset_server.load("audio.mp3"), &channel);
-    /// }
-    /// ```
     pub fn new(key: String) -> Self {
         AudioStreamChannel { key }
     }
