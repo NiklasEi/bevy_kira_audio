ef4b6b869d714a0d41c6a6bff2b69fca5c894166
Niklas Eicker <git@nikl.me>
2023-06-24 13:36:09 +0200

    Fix settings loader for Kira 0.8

--- src/source/settings_loader.rs
+++ src/source/settings_loader.rs
@@ -4,8 +4,9 @@ use std::{io::Cursor, path::PathBuf};
 use bevy::asset::{AssetLoader, LoadContext, LoadedAsset};
 use bevy::utils::BoxedFuture;
 use kira::sound::static_sound::{StaticSoundData, StaticSoundSettings};
+use kira::sound::{EndPosition, PlaybackPosition, PlaybackRate, Region};
 use kira::tween::Tween;
-use kira::{LoopBehavior, PlaybackRate, Volume};
+use kira::Volume;
 use serde::Deserialize;
 
 use crate::AudioSource;
@@ -23,14 +24,20 @@ struct SoundSettings {
     /// Location of the sound file.
     file: PathBuf,
 
-    /// The initial playback position of the sound (in seconds).
+    /// The region of the sound that should be played.
+    #[serde(default = "default_full_file")]
+    pub playback_region: Region,
+
+    /// The portion of the sound that should be looped.
     #[serde(default)]
-    pub start_position: f64,
+    pub loop_region: Option<Region>,
+
     /// Amplitude multiplier
     ///
     /// If the channel you play the sound is configured, it will overwrite the volume here.
     #[serde(default = "default_one")]
     pub volume: f64,
+
     /// The playback rate of the sound.
     ///
     /// Changing the playback rate will change both the speed
@@ -39,29 +46,33 @@ struct SoundSettings {
     /// If the channel you play the sound is configured, it will overwrite the volume here.
     #[serde(default = "default_one")]
     pub playback_rate: f64,
+
     /// The panning of the sound, where 0 is hard left
     /// and 1 is hard right.
     ///
     /// If the channel you play the sound is configured, it will overwrite the volume here.
     #[serde(default = "default_panning")]
     pub panning: f64,
+
     /// Whether the sound should play in reverse.
     ///
     /// If set to `true`, the start position will be relative
     /// to the end of the sound.
     #[serde(default)]
     pub reverse: bool,
-    /// The looping behavior of the sound.
-    ///
-    /// If you set a value here, playback will jump to that position when the end of the sound
-    /// has been reached.
-    #[serde(default)]
-    pub loop_behavior: Option<f64>,
+
     /// An optional linear fade-in from silence.
     ///
     /// The [`u64`] value is the duration of the tween in milliseconds.
     #[serde(default)]
     pub fade_in_tween: Option<u64>,
+}
+
+fn default_full_file() -> Region {
+    Region {
+        start: PlaybackPosition::Samples(0),
+        end: EndPosition::EndOfAudio,
+    }
 }
 
 fn default_one() -> f64 {
@@ -76,14 +87,12 @@ impl From<SoundSettings> for StaticSoundSettings {
     fn from(settings: SoundSettings) -> Self {
         let mut static_sound_settings = StaticSoundSettings::new();
 
-        static_sound_settings.start_position = settings.start_position;
-        static_sound_settings.volume = Volume::from(settings.volume);
-        static_sound_settings.playback_rate = PlaybackRate::from(settings.playback_rate);
-        static_sound_settings.panning = settings.panning;
+        static_sound_settings.playback_region = settings.playback_region;
+        static_sound_settings.volume = Volume::from(settings.volume).into();
+        static_sound_settings.playback_rate = PlaybackRate::from(settings.playback_rate).into();
+        static_sound_settings.panning = settings.panning.into();
         static_sound_settings.reverse = settings.reverse;
-        static_sound_settings.loop_behavior = settings
-            .loop_behavior
-            .map(|start_position| LoopBehavior { start_position });
+        static_sound_settings.loop_region = settings.loop_region;
         static_sound_settings.fade_in_tween = settings.fade_in_tween.map(|micros| Tween {
             duration: Duration::from_micros(micros),
             ..Default::default()
